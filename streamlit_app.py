06225f22f6cb102b79a8a7afb1c9246ec939d8d0
HassanSeaspan <Hassan.Elghayaty@seaspan.com>
2024-11-07 10:55:35 -0800

    Update streamlit_app.py with the get images script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import Get_Images_Script.py
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Streamlit App with External Script")
+
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
