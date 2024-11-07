d039033ef04ccbe77059ccebc48909344ce4fa07
Hassan Elghayaty <Hassan.Elghayaty@seaspan.com>
2024-11-07 13:51:27 -0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import Get_Images_Web  # This imports your script
 
 # Your Streamlit app code
-st.title("🎈 Streamlit Web App with External Script")
+# st.title("🎈 Streamlit Web App with External Script")
 
 # Example: If Get_Images_Web.py has a function `main` that runs the script
 Get_Images_Web.main()  # Calls the main function from Get_Images_Web.py
